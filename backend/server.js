476188bd251570c578bd4de0517628a08399f1b8
sgw8 <vasylmiskiv@gmail.com>
2022-06-17 00:19:44 +0300

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,29 @@
-import express from'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import colors from 'colors'
-import goodsRoute from './routes/goodsRoute.js'
-import path from 'path'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import goodsRoute from "./routes/goodsRoute.js";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = new express()
+const app = new express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/goods', goodsRoute)
+app.use("/api/goods", goodsRoute);
 
 //static folder
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'mern', 'build', 'index.html')))
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "mern", "build", "index.html"))
+);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started! :${PORT}`.yellow.italic))
+app.listen(PORT, console.log(`Server started! :${PORT}`.yellow.italic));
